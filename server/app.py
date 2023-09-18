1b27bba5afb48feb8b9bb5ccec27e29785c1dd89
Donell <donell.wambua@student.moringaschool.com>
2023-09-18 13:17:22 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3
-
 from flask import Flask
+
+
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(host='localhost', port=5555)
+
